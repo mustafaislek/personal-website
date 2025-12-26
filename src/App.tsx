09e4f89d5a7deefcc8cb108ef6e9eba1b1c85bc2
mustafaislek <islekmustafaa@gmail.com>
2025-12-27 02:17:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+        <div className="space-y-6">
+          <h1 className="text-2xl font-bold logo ">
+            Hey, I'm Mustafa, a developer who enjoys building things.
+          </h1>
+          <p>
+            An engineer with over 5 years of software development experience, I am passionate about
+            problem-solving, constantly learning, creating, and sharing things using the knowledge I
+            acquire. I have a strong motivation to continuously improve myself and thrive on tackling
+            bigger and more complex problems.
+          </p>
+        <footer>
+          {` • `}
+          <a href="https://linkedin.com/in/mustafaislek/" target="_blank">
+            LinkedIn
+          </a>
+          {` • `}
+          <a href="https://github.com/mustafaislek/" target="_blank">
+            GitHub
+          </a>
+        </footer>
+        </div>
+
+    </>
+  )
+}
+
+export default App
