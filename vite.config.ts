1d2d49b6b90a00ed8e1ea62584d9584f2c403805
mustafaislek <islekmustafaa@gmail.com>
2025-12-27 03:26:25 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    host: true,
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 })
