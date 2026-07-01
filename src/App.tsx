a2800c7954e828fd7143acb7c7ad7875a235bf64
Mustafa İşlek <33036930+mustafaislek@users.noreply.github.com>
2026-07-01 15:48:19 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
             Hey, I'm Mustafa, a developer who enjoys building things.
           </h1>
           <p>
-            An engineer with over 7 years of software development experience, I am passionate about
+            An engineer with over 8 years of software development experience, I am passionate about
             problem-solving, constantly learning, creating, and sharing things using the knowledge I
             acquire. I have a strong motivation to continuously improve myself and thrive on tackling
             bigger and more complex problems.
